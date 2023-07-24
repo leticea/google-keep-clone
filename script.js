65d39bca2b1bbadef792d3cb4e16a037a2209f1a
leticea <nascimento.leticeah@gmail.com>
2023-07-24 15:36:37 -0300

    feat(script.js): addNote function

---
+++ script.js
@@ -0,0 +1,9 @@
+const notesContainer = document.querySelector("#notes-container");
+const noteInput = document.querySelector("#note-container");
+const addNoteBtn = document.querySelector(".add-note");
+
+function addNote() {
+  console.log("testando");
+}
+
+addNoteBtn.addEventListener("click", () => addNote());
