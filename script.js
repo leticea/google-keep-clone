bbaf8c500d880d3e02419eb6f141338500d5cf73
leticea <nascimento.leticeah@gmail.com>
2023-07-24 15:47:59 -0300

    feat(script.js): createNote function

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const notesContainer = document.querySelector("#notes-container");
-const noteInput = document.querySelector("#note-container");
+const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 
 function addNote() {
-  console.log("testando");
+  const noteObject = {
+    id: generateId(),
+    content: noteInput.value,
+    fixed: false,
+  };
+
+  const noteElement = createNote(noteObject.id, noteObject.content);
+}
+
+function generateId() {
+  return Math.floor(Math.random() * 5000);
+}
+
+function createNote(id, content, fixed) {
+  const element = document.createElement("div");
+  element.classList.add("note");
+
+  const textarea = document.createElement("textarea");
+  textarea.value = content;
+  textarea.placeholder = "Adicione algum texto...";
+
+  element.appendChild(textarea);
+
+  return element;
 }
 
 addNoteBtn.addEventListener("click", () => addNote());
